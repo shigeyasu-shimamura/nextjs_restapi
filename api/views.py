5c587ea226fa177ce361d092c3fc3569169fd779
shimamura <sgys.mtk@gmail.com>
2021-07-28 14:30:01 +0900

    initial commit

---
+++ api/views.py
@@ -0,0 +1,44 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+from rest_framework.permissions import AllowAny
+from rest_framework import generics
+from rest_framework import viewsets
+from .serializers import TaskSerializer, UserSerializer, PostSerializer
+from .models import Task, Post
+
+
+class CreateUserView(generics.CreateAPIView):
+    serializer_class = UserSerializer
+    permission_classes = (AllowAny,)
+
+
+class PostListView(generics.ListAPIView):
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
+    permission_classes = (AllowAny,)
+
+class PostRetrieveView(generics.RetrieveAPIView):
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
+    permission_classes = (AllowAny,)
+
+
+class TaskListView(generics.ListAPIView):
+    queryset = Task.objects.all()
+    serializer_class = TaskSerializer
+    permission_classes = (AllowAny,)
+
+class TaskRetrieveView(generics.RetrieveAPIView):
+    queryset = Task.objects.all()
+    serializer_class = TaskSerializer
+    permission_classes = (AllowAny,)
+
+
+
+# ModelViewSetを利用→CRUDを利用できる
+class TaskViewSet(viewsets.ModelViewSet):
+    queryset = Task.objects.all()
+    serializer_class = TaskSerializer
+
